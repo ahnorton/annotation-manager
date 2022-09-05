30c148b888785b92d17338b2ba54d19df38da713
Andrew Norton <norton.ah@gmail.com>
2022-09-05 14:06:59 +1000

    Add files via upload

--- controls_file.py
+++ controls_file.py
@@ -1,41 +1,56 @@
 
+# The idea is to separate a GUI program and main program by having them read/write a controls file.
+# Importing this module to a GUI program and main program makes this easy to do.
+#
+# Usage:
+# from controls_file import *   
+
+__all__ = [
+    'controls_file',
+    'controls_write',
+    'controls_read',
+    'controls_modified_q',
+    'controls_set',
+    'controls_get' ]
+
 import pickle
 import os
-
 
 # -----------------------------------------------------------------------------------------
 # Location constants.
 
 # Default, 
-controls_file = 'controls.dat'
+cfile = 'controls.dat'
 
-# To change the default,
-def controls_file_location(f):
-    global controls_file
-    controls_file = f
+def controls_file(f):
+    global cfile
+    cfile = f
     
 # -----------------------------------------------------------------------------------------
 
+# Initially,
 controls_dict = {}
 controls_mtime = 0                
 
 def controls_write():
-    global controls_file, controls_dict, controls_mtime
-    with open(controls_file, 'wb') as g:
+    global cfile, controls_dict, controls_mtime
+    dir = os.path.dirname(os.path.realpath(cfile))
+    os.makedirs(dir, exist_ok = True)
+    with open(cfile, 'wb') as g:
         pickle.dump(controls_dict, g, protocol=0)  # 0 => human readable.
-    controls_mtime = os.path.getmtime(controls_file)
+    controls_mtime = os.path.getmtime(cfile)
 
 def controls_read():                   
-    global controls_file, controls_dict
+    global cfile, controls_dict
     try:
-        with open(controls_file, 'rb') as g:
+        with open(cfile, 'rb') as g:
             controls_dict = pickle.load(g)
     except:
         controls_dict = {} 
 
 def controls_modified_q():
-    global controls_file, controls_mtime
-    mtime = os.path.getmtime(controls_file)    
+    global cfile, controls_mtime
+    mtime = os.path.getmtime(cfile)    
     ans = not (mtime == controls_mtime)
     if ans:
        controls_mtime = mtime 
